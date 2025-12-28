c56c9cfb52ae6b662d34743f83448f7c1193e118
OluC94 <coker.olu94@gmail.com>
2025-12-28 19:06:22 +0000

    initial commit

---
+++ src/main/java/com/nc_games/NcGamesApplication.java
@@ -0,0 +1,13 @@
+package com.nc_games;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NcGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NcGamesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nc_games/NcGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nc_games;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NcGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
